61aff3ed7d5668fcec2fbd77587beb94e1f1cd25
Todd H <github@simplybest.com>
2023-04-21 10:40:40 -0600

    day 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  constructor() { }
 
-  constructor() {}
+  alertButtons = [
+    {
+      text: 'OK',
+      handler: this.myHandler
+    }
+  ];
+
+  person = {
+    name: 'Todd',
+    nickname: '',
+    age: '',
+    bio: '',
+  };
+
+  public alertInputs = [
+    {
+      placeholder: 'Name',
+      value: this.person.name,
+    },
+    {
+      placeholder: 'Nickname (max 8 characters)',
+      attributes: {
+        maxlength: 8,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'Age',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'A little about yourself',
+    },
+  ];
+
+  myHandler(ev: Event) {
+    console.log('Alert did dismiss');
+  }
 
 }
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import {HttpClientModule} from '@angular/common/http';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab3PageRoutingModule
+    Tab3PageRoutingModule,
+    HttpClientModule
   ],
   declarations: [Tab3Page]
 })
